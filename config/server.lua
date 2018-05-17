67d55c18e5895c68f7f50e53b77099638dadad79
Xander Harrison <xander.harrison77@gmail.com>
2018-05-17 13:03:53 -0500

    Added version checker

--- config/server.lua
+++ config/server.lua
@@ -1,6 +1,6 @@
 XRPLifeConfig["server"].checkXRPVersion = true
 XRPLifeConfig["server"].debugMode = true
-XRPLifeConfig["server"].localChat = false
+XRPLifeConfig["server"].localChat = true
 XRPLifeConfig["server"].localChatDistance = 20.0
 
 XRPLifeServer.Helpers.DebugMessage("Config - server.lua Loaded")
--- start.lua
+++ start.lua
@@ -1,4 +1,59 @@
 ---------------------------------------------------------------------------
+-- Version Checker
+---------------------------------------------------------------------------
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+Citizen.CreateThread(function()
+    print("---------------------------------------------------------------------------\n")
+    print(" Welcome to XRPLife made by [Xander1998 (X. Cross)]\n")
+    print(" Version Checker is " .. tostring(XRPLifeConfig["server"].checkXRPVersion) .. "\n")
+    print(" Debug mode is " .. tostring(XRPLifeConfig["server"].debugMode) .. "\n")
+    print("---------------------------------------------------------------------------\n")
+
+    if XRPLifeConfig["server"].checkXRPVersion then
+
+        PerformHttpRequest("https://api.github.com/repos/xander1998/xrplife/contents/version.json", function(code, text, something)
+            
+            local data = json.decode(text)
+            local versionData = json.decode(decode(data.content))
+
+            local jsonVersion = json.decode(LoadResourceFile(GetCurrentResourceName(), "version.json"))
+            
+            if code == 200 then
+                print("---------------------------------------------------------------------------\n")
+                if tonumber(versionData.version) == jsonVersion.version then
+                    print(" XRPLife Version Updated and ready to go. Version: " .. jsonVersion.version .. "\n")
+                    print("---------------------------------------------------------------------------\n")
+                else
+                    print(" XRPLife version does NOT match. Think about updating?\n Your Version: " .. jsonVersion.version .. "\n Newest Version: " .. versionData.version .. "\n")
+                    print("---------------------------------------------------------------------------\n")
+                end
+            else
+                print(" Error checking version API. You don't have to worry about this. It just didn't check for any updates... All good to go!\n")
+                print("---------------------------------------------------------------------------\n")
+            end
+
+        end, "GET", json.encode({}), {["Content-Type"] = "application/text"})
+
+    end
+end)
+
+function decode(data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+---------------------------------------------------------------------------
 -- Handles players connecting to the server
 ---------------------------------------------------------------------------
 AddEventHandler("playerConnecting", function(name, reason, deferrals)
